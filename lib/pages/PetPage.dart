7aa3a879a797e04cac406890a3e89461b8575f0f
nakaren <nakaren1234@gmail.com>
2020-10-23 14:11:15 +0700

    ทำการเทดข้อมูลว่าดึงไง
    ถ้าจะดึงให้สร้างclassแยกแล้วดึงมาใช้

--- lib/pages/PetPage.dart
+++ lib/pages/PetPage.dart
@@ -1,4 +1,9 @@
+// import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutterapp/widgets/ShowDetailUser.dart';
+// import 'package:flutterapp/widgets/ShowListUser.dart';
+// import 'package:flutterapp/widgets/addListUser.dart';
+// import 'package:flutterapp/models/userprofile_model.dart';
 
 class PetPage extends StatefulWidget {
   PetPage({Key key}) : super(key: key);
@@ -8,16 +13,52 @@ class PetPage extends StatefulWidget {
 }
 
 class _PetPageState extends State<PetPage> {
+  //field
+  // List<UserprofileModel> userprofileModels = List();
+
+  Widget petWidget = ShowDetailUser();
+
+  //method
+
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   readAllData();
+  // }
+
+  // Future<void> readAllData() async {
+  //   // FirestoreFirestore firestore = FirestoreFirestore.instance;
+  //   FirebaseFirestore firestore = FirebaseFirestore.instance;
+  //   CollectionReference collectionReference =
+  //       firestore.collection('Userprofile');
+  //   // ignore: await_only_futures
+  //   await collectionReference.snapshots().listen((response) {
+  //     // ignore: deprecated_member_use
+  //     List<DocumentSnapshot> snapshots = response.documents;
+  //     for (var snapshot in snapshots) {
+  //       print('snapshot = $snapshot');
+  //       print('Name = ${snapshot.data['Name']}');
+  //       // print(" = ${snapshot.data['Detail']}");
+  //       // print('Name = ${snapshot.data['Name']}');
+
+  //       UserprofileModel userprofileModel =
+  //           UserprofileModel.fromMap(snapshot.data);
+  //       setState(() {
+  //         userprofileModels.add(userprofileModel);
+  //       });
+  //     }
+  //   });
+  // }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.pinkAccent,
-          title: Text('ประวัติสัตว์เลี้ยง'),
-          actions: [],
-        ),
-        body: SingleChildScrollView(
-          child: Text('Hello'),
-        ));
+      appBar: AppBar(
+        backgroundColor: Colors.pinkAccent,
+        title: Text('ประวัติสัตว์เลี้ยง'),
+        actions: [],
+      ),
+      body: petWidget,
+    );
   }
 }
---
+++ lib/widgets/ShowDetailUser.dart
@@ -0,0 +1,63 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:flutterapp/models/userprofile_model.dart';
+
+class ShowDetailUser extends StatefulWidget {
+  ShowDetailUser({Key key}) : super(key: key);
+
+  @override
+  _ShowDetailUserState createState() => _ShowDetailUserState();
+}
+
+class _ShowDetailUserState extends State<ShowDetailUser> {
+  // Field
+  List<UserprofileModel> userprofileModels = List();
+
+  // Method
+
+  @override
+  void initState() {
+    super.initState();
+    readAllData();
+  }
+
+  Future<void> readAllData() async {
+    // FirestoreFirestore firestore = FirestoreFirestore.instance;
+    FirebaseFirestore firestore = FirebaseFirestore.instance;
+    CollectionReference collectionReference =
+        firestore.collection('Userprofile');
+    // ignore: await_only_futures
+    await collectionReference.snapshots().listen((response) {
+      // ignore: deprecated_member_use
+      List<DocumentSnapshot> snapshots = response.documents;
+      for (var snapshot in snapshots) {
+        print('snapshot = $snapshot');
+        print('Name = ${snapshot.data['Name']}');
+        // print(" = ${snapshot.data['Detail']}");
+        // print('Name = ${snapshot.data['Name']}');
+
+        UserprofileModel userprofileModel =
+            UserprofileModel.fromMap(snapshot.data);
+        setState(() {
+          userprofileModels.add(userprofileModel);
+        });
+      }
+    });
+  }
+
+  Widget showName(int index) {
+    return Text(userprofileModels[index].name);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: ListView.builder(
+        itemCount: userprofileModels.length,
+        itemBuilder: (BuildContext buildContext, int index) {
+          return showName(index);
+        },
+      ),
+    );
+  }
+}
