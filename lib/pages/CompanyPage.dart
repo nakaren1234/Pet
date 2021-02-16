55d95f6af41a59ef33ddb379e5a97a7a4a7f5b8a
nakaren meechom <nakaren1234@gmail.com>
2021-02-16 16:01:56 +0700

    อัพyma

--- lib/pages/CompanyPage.dart
+++ lib/pages/CompanyPage.dart
@@ -69,7 +69,7 @@ class _CompanyPageState extends State<CompanyPage> {
                       ),
                     ),
                     Divider(color: Colors.black),
-                    buildFooter()
+                    buildFooter(),
                   ],
                 ),
               ),
